b5512d456c7f82973a72a4dbbd4a9552403db7c2
ImageResizePro <vansh56v@gmail.com>
2025-04-11 11:59:12 +0530

    adsterra-loader.js

---
+++ adsterra-loader.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  const adContainer = document.createElement('div');
+  adContainer.id = 'container-5d120834528f816ae0581b74b95f4bbe'; 
+
+  const target = document.getElementById('download-button') || document.body;
+  target.parentNode.insertBefore(adContainer, target.nextSibling);
+
+  const adScript = document.createElement('script');
+  adScript.src = '//www.highperformanceformat.com/5d120834528f816ae0581b74b95f4bbe/invoke.js"></script>'; 
+  adScript.async = true;
+  adScript.setAttribute('data-cfasync', 'false');
+
+  document.body.appendChild(adScript);
+};
